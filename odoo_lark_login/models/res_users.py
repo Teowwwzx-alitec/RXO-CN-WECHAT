6c443ee14e178692e2c4a6d66030212e9df0929c
Teowwwzx-alitec <zhenxiang.teow@alitec.asia>
2025-05-06 19:46:19 +0800

    Update res_users.py

--- odoo_lark_login/models/res_users.py
+++ odoo_lark_login/models/res_users.py
@@ -196,7 +196,7 @@ class ResUsers(models.Model):
             try:
                 user.write({
                     "openid": open_id,
-                    "oauth_access_token": lark_access_token,
+                    # "oauth_access_token": lark_access_token,
                 })
                 _logger.info("Final write executed for user %s, setting openid and oauth_access_token.", user.id)
                 _logger.info("Final write executed for ", lark_access_token)
